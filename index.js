ea9cd411f1f1fe912282c651263b54307486983e
Arthur Lam <lam.arthur0311@gmail.com>
2020-01-16 22:15:57 -0500

    implemented signin signout with secured user password hashing

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(
   })
 );
 
-app.get('/', (req, res) => {
+app.get('/signup', (req, res) => {
   res.send(`
     <div>
       Your id is: ${req.session.userId}
@@ -26,7 +26,7 @@ app.get('/', (req, res) => {
   `);
 });
 
-app.post('/', async (req, res) => {
+app.post('/signup', async (req, res) => {
   const { email, password, passwordConfirmation } = req.body;
 
   const existingUser = await usersRepo.getOneBy({ email });
@@ -43,6 +43,46 @@ app.post('/', async (req, res) => {
   req.session.userId = user.id;
 
   res.send('Account created.');
+});
+
+app.get('/signin', (req, res) => {
+  res.send(`
+    <div>
+      <form method='POST'>
+        <input name='email' placeholder='email' />
+        <input name='password' placeholder='password' />
+        <button>Sign In</button>
+      </form>
+    </div>
+    `);
+});
+
+app.post('/signin', async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await usersRepo.getOneBy({ email });
+
+  if (!user) {
+    return res.send('Email not found');
+  }
+
+  const validPassword = await usersRepo.comparePasswords(
+    user.password,
+    password
+  );
+
+  if (!validPassword) {
+    return res.send('Invalid password');
+  }
+
+  req.session.userId = user.id;
+
+  res.send('You are signed in');
+});
+
+app.get('/signout', (req, res) => {
+  req.session = null;
+  res.send('You are logged out');
 });
 
 app.listen(3000, () => {
--- repositories/users.js
+++ repositories/users.js
@@ -1,5 +1,8 @@
 const fs = require('fs');
 const crypto = require('crypto');
+const util = require('util');
+
+const scrypt = util.promisify(crypto.scrypt);
 
 class UsersRepository {
   constructor(filename) {
@@ -49,12 +52,19 @@ class UsersRepository {
   async create(attrs) {
     attrs.id = this.randomId();
 
+    const salt = crypto.randomBytes(8).toString('hex');
+    const buffer = await scrypt(attrs.password, salt, 64);
+
     const records = await this.getAll();
-    records.push(attrs);
+    const record = {
+      ...attrs,
+      password: `${buffer.toString('hex')}.${salt}`
+    };
+    records.push(record);
 
     await this.writeAll(records);
 
-    return attrs;
+    return record;
   }
 
   async update(id, attrs) {
@@ -80,6 +90,15 @@ class UsersRepository {
       this.filename,
       JSON.stringify(records, null, 2)
     );
+  }
+
+  async comparePasswords(saved, supplied) {
+    // saved > password stored in database. 'hashed.salt'
+    // supplied > password user entered for trying to sign in
+    const [hashed, salt] = saved.split('.');
+    const hashedSuppliedBuffer = await scrypt(supplied, salt, 64);
+
+    return hashed === hashedSuppliedBuffer.toString('hex');
   }
 
   randomId() {
