2d0b155707d8f196ffc631cfff46caf40e52247a
Arthur Lam <lam.arthur0311@gmail.com>
2020-01-17 14:01:56 -0500

    set up admin panel router

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
 const authRouter = require('./routes/admin/auth');
-
+const productsRouter = require('./routes/admin/products');
 const app = express();
 
 app.use(express.static('public'));
@@ -14,6 +14,7 @@ app.use(
 );
 
 app.use(authRouter);
+app.use(productsRouter);
 
 app.listen(3000, () => {
   console.log('Web server is connected.');
