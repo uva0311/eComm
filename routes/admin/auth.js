8d979c237f79c19cc00115fd67dbfe37e70da12e
Arthur Lam <lam.arthur0311@gmail.com>
2020-01-18 13:14:35 -0500

    refractor to extract error handling function to another file

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const { check, validationResult } = require('express-validator');
 
+const { handleErrors } = require('./middlewares');
 const usersRepo = require('../../repositories/users');
 const signupTemplate = require('../../views/admin/auth/signup');
 const signinTemplate = require('../../views/admin/auth/signin');
@@ -21,14 +21,9 @@ router.get('/signup', (req, res) => {
 router.post(
   '/signup',
   [requireEmail, requirePassword, requirePasswordConfirmation],
+  handleErrors(signupTemplate),
   async (req, res) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      return res.send(signupTemplate({ req, errors }));
-    }
-
-    const { email, password, passwordConfirmation } = req.body;
+    const { email, password } = req.body;
     // create an new user and give new user an associated id
     const user = await usersRepo.create({ email, password });
 
@@ -46,12 +41,8 @@ router.get('/signin', (req, res) => {
 router.post(
   '/signin',
   [requireEmailExist, requireValidPasswordForUser],
+  handleErrors(signinTemplate),
   async (req, res) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      return res.send(signinTemplate({ errors }));
-    }
     const { email } = req.body;
 
     const user = await usersRepo.getOneBy({ email });
