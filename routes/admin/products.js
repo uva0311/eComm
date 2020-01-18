6254d1f389067b1145cd4862a3531a562b986637
Arthur Lam <lam.arthur0311@gmail.com>
2020-01-18 13:40:31 -0500

    modified to include extra authentication to make sure only logged in users can create new products

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,22 +1,27 @@
 const express = require('express');
 const multer = require('multer');
 
-const { handleErrors } = require('./middlewares');
+const { handleErrors, requireAuth } = require('./middlewares');
 const productsRepo = require('../../repositories/products');
 const productsNewTemplate = require('../../views/admin/products/new');
+const productIndexTemplate = require('../../views/admin/products/index');
 const { requireTitle, requirePrice } = require('./validators');
 
 const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage() });
 
-router.get('/admin/products', (req, res) => {});
+router.get('/admin/products', requireAuth, async (req, res) => {
+  const products = await productsRepo.getAll();
+  res.send(productIndexTemplate({ products }));
+});
 
-router.get('/admin/products/new', (req, res) => {
+router.get('/admin/products/new', requireAuth, (req, res) => {
   res.send(productsNewTemplate({}));
 });
 
 router.post(
   '/admin/products/new',
+  requireAuth,
   upload.single('image'),
   [requireTitle, requirePrice],
   handleErrors(productsNewTemplate),
@@ -26,7 +31,7 @@ router.post(
 
     await productsRepo.create({ title, price, image });
 
-    res.send('submitted');
+    res.redirect('/admin/products');
   }
 );
 
