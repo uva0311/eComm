1f0524543d386c6ad2db9fb4d01270bf01d7ffa9
Arthur Lam <lam.arthur0311@gmail.com>
2020-01-19 13:58:23 -0500

    implemented view shopping cart route

--- routes/carts.js
+++ routes/carts.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cartsRepo = require('../repositories/carts');
+const productsRepo = require('../repositories/products');
+const cartShowTemplate = require('../views/carts/show');
 
 const router = express.Router();
 
@@ -34,7 +36,21 @@ router.post('/cart/products', async (req, res) => {
 });
 
 // receive a get request to show all items in cart
+router.get('/cart', async (req, res) => {
+  if (!req.session.cartId) {
+    res.redirect('/');
+  }
 
+  const cart = await cartsRepo.getOne(req.session.cartId);
+
+  for (let item of cart.items) {
+    const product = await productsRepo.getOne(item.id);
+
+    item.product = product;
+  }
+
+  res.send(cartShowTemplate({ items: cart.items }));
+});
 // receive a psot request to delete an item from a cart
 
 module.exports = router;
