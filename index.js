f3ef14a5dd232baf97ad5947acd1f67623ecda44
Arthur Lam <lam.arthur0311@gmail.com>
2020-01-19 12:24:31 -0500

    updated to include products route

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
 const authRouter = require('./routes/admin/auth');
-const productsRouter = require('./routes/admin/products');
+const adminProductsRouter = require('./routes/admin/products');
+const productsRouter = require('./routes/products');
+
 const app = express();
 
 app.use(express.static('public'));
@@ -14,6 +16,7 @@ app.use(
 );
 
 app.use(authRouter);
+app.use(adminProductsRouter);
 app.use(productsRouter);
 
 app.listen(3000, () => {
